2ed000a811fa96578a2b94bc1df3950f0f4ef889
Lucas Denipoti <lucasfdenipoti@gmail.com>
2020-09-10 18:27:49 -0300

    código finalizado

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+	public class Produto
+	{
+		public string Nome;
+		public double Preço;
+		public int Quantidade;
+	}
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace ConsoleApp1
 {
@@ -6,7 +7,35 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Produto n, p, q;
+            n = new Produto();
+            p = new Produto();
+            q = new Produto();
+
+            Console.WriteLine("Entre com os produtos:");
+            Console.Write("Nome: ");
+            n.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.Preço = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade em estoque: ");
+            q.Quantidade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double ValorTotalEstoque = (p.Preço * q.Quantidade);
+            Console.WriteLine("Dados do produto: " + n.Nome + ", R$" + p.Preço + ", " + q.Quantidade + " unidades, total: R$" + ValorTotalEstoque);
+
+            Console.Write("Digite o número a ser adicionado no estoque: ");
+            int AdicionarProdutos = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            q.Quantidade = q.Quantidade + AdicionarProdutos;
+            ValorTotalEstoque = (p.Preço * q.Quantidade);
+            Console.WriteLine("Dados atualizados do produto: " + n.Nome + ", R$" + p.Preço + ", " + q.Quantidade + " unidades, total: R$" + ValorTotalEstoque);
+
+            Console.Write("Digite o número a ser retirado no estoque: ");
+            int RemoverProdutos = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            q.Quantidade = q.Quantidade - RemoverProdutos;
+            ValorTotalEstoque = p.Preço * q.Quantidade;
+            Console.WriteLine("Dados atualizados do produto: " + n.Nome + ", R$" + p.Preço + ", " + q.Quantidade + " unidades, total: R$" + ValorTotalEstoque);
+
+            Console.ReadKey();
         }
     }
 }
