93935dc767e8548c44a9869dacb025dd8e49afdc
Lucas Denipoti <lucasfdenipoti@gmail.com>
2020-09-16 16:39:29 -0300

    finalizei as mudanças do ToString na classe e no main

--- ConsoleApp1/ConsoleApp1/Class1.cs
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Globalization;
 
 namespace ConsoleApp1
 {
@@ -9,5 +10,28 @@ namespace ConsoleApp1
 		public string Nome;
 		public double Preço;
 		public int Quantidade;
+
+		public double ValorTotalEmEstoque()
+		{
+			return Preço * Quantidade;
+		}
+		public void AddProdutos(int quantidade)
+		{
+			Quantidade += quantidade;
+		}
+		public void SubProdutos(int quantidade)
+		{
+			Quantidade -= quantidade;
+		}
+		public override string ToString()
+		{
+			return Nome
+				+ ", $"
+				+ Preço.ToString("F2", CultureInfo.InvariantCulture)
+				+ ", "
+				+ Quantidade
+				+ " unidades, Total: $ "
+				+ ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+		}
 	}
 }
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,33 +7,25 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Produto n, p, q;
-            n = new Produto();
-            p = new Produto();
-            q = new Produto();
-
+            Produto p = new Produto();
             Console.WriteLine("Entre com os produtos:");
             Console.Write("Nome: ");
-            n.Nome = Console.ReadLine();
+            p.Nome = Console.ReadLine();
             Console.Write("Preço: ");
             p.Preço = double.Parse(Console.ReadLine() ,CultureInfo.InvariantCulture);
             Console.Write("Quantidade em estoque: ");
-            q.Quantidade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-            double ValorTotalEstoque = (p.Preço * q.Quantidade);
-            Console.WriteLine("Dados do produto: " + n.Nome + ", R$" + p.Preço.ToString("F2", CultureInfo.InvariantCulture) + ", " + q.Quantidade + " unidades, total: R$" + ValorTotalEstoque.ToString("F2", CultureInfo.InvariantCulture));
+            p.Quantidade = int.Parse(Console.ReadLine());
+            Console.WriteLine("Dados do produto: " + p);
 
             Console.Write("Digite o número a ser adicionado no estoque: ");
-            int AdicionarProdutos = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            q.Quantidade = q.Quantidade + AdicionarProdutos;
-            ValorTotalEstoque = (p.Preço * q.Quantidade);
-            Console.WriteLine("Dados atualizados do produto: " + n.Nome + ", R$" + p.Preço.ToString("F2", CultureInfo.InvariantCulture) + ", " + q.Quantidade + " unidades, total: R$" + ValorTotalEstoque.ToString("F2", CultureInfo.InvariantCulture));
+            int AdicionarProdutos = int.Parse(Console.ReadLine());
+            p.AddProdutos(AdicionarProdutos);
+            Console.WriteLine("Dados atualizados do produto: " + p);
 
             Console.Write("Digite o número a ser retirado no estoque: ");
-            int RemoverProdutos = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            q.Quantidade = q.Quantidade - RemoverProdutos;
-            ValorTotalEstoque = p.Preço * q.Quantidade;
-            Console.WriteLine("Dados atualizados do produto: " + n.Nome + ", R$" + p.Preço.ToString("F2", CultureInfo.InvariantCulture) + ", " + q.Quantidade + " unidades, total: R$" + ValorTotalEstoque.ToString("F2", CultureInfo.InvariantCulture));
+            int RemoverProdutos = int.Parse(Console.ReadLine());
+            p.SubProdutos(RemoverProdutos);
+            Console.WriteLine("Dados atualizados do produto: " + p);
 
             Console.ReadKey();
         }
